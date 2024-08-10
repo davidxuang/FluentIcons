6d991ac5438b3c9a2c40ee010cce0f7acffe145d
David Huang <davidxuang@live.com>
2024-08-10 12:19:01 +0800

    Fix reproducible TTF build

--- seagull-icons/build.ps1
+++ seagull-icons/build.ps1
@@ -24,8 +24,8 @@ try {
     python "$PSScriptRoot/patch.py" "$PSScriptRoot/obj/FluentSystemIcons.ttf" "$PSScriptRoot/obj/codepoints.json"
     python "$PSScriptRoot/patch.py" "$PSScriptRoot/obj/SeagullFluentIcons.ttf" "$PSScriptRoot/obj/codepoints.json"
 
-    ttx -m "$PSScriptRoot/obj/FluentSystemIcons.ttf" -o "$PSScriptRoot/obj/FluentSystemIcons.ttf" "$PSScriptRoot/obj/colr/colr.ttx"
-    ttx -m "$PSScriptRoot/obj/SeagullFluentIcons.ttf" -o "$PSScriptRoot/obj/SeagullFluentIcons.ttf" "$PSScriptRoot/obj/colr/colr.ttx"
+    ttx -m "$PSScriptRoot/obj/FluentSystemIcons.ttf" -o "$PSScriptRoot/obj/FluentSystemIcons.ttf" --no-recalc-timestamp "$PSScriptRoot/obj/colr/colr.ttx"
+    ttx -m "$PSScriptRoot/obj/SeagullFluentIcons.ttf" -o "$PSScriptRoot/obj/SeagullFluentIcons.ttf" --no-recalc-timestamp "$PSScriptRoot/obj/colr/colr.ttx"
     Copy-Item "$PSScriptRoot/obj/FluentSystemIcons.ttf" "$PSScriptRoot/assets" -Force
     Copy-Item "$PSScriptRoot/obj/SeagullFluentIcons.ttf" "$PSScriptRoot/assets" -Force
 } finally {
