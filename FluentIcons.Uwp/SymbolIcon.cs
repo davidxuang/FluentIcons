6b45001dade9475d36aab88ef9e0d73977ddd3fb
devsko <devsko@users.noreply.github.com>
2024-11-13 01:32:02 +0100

    Add support for Modern UWP (#12)
    * Fixing uap content files
    
    * Add modern UWP, add font resource content files manually
    
    * Fix AOT
    
    * Only copy fonts to executables and don't bother packaging projects
    
    * Make platform check more generic
    
    * Asset path changed
    
    ---------
    
    Co-authored-by: Dawei Huang <davidxuang@live.com>

--- FluentIcons.Uwp/SymbolIcon.cs
+++ FluentIcons.Uwp/SymbolIcon.cs
@@ -154,7 +154,7 @@ public partial class SymbolIcon : FontIcon
 }
 
 [MarkupExtensionReturnType(ReturnType = typeof(SymbolIcon))]
-public class SymbolIconExtension : MarkupExtension
+public partial class SymbolIconExtension : MarkupExtension
 {
     public Symbol? Symbol { get; set; }
     public IconVariant? IconVariant { get; set; }
