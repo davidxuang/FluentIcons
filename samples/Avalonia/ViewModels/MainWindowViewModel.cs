bca8a563ea20db496ebb5dcb086c700b18db2827
FantasyTeddy <matthiasbaer@bluewin.ch>
2026-07-02 17:35:37 +0200

    Add an Avalonia sample application

---
+++ samples/Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluentIcons.Samples.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public IEnumerable<Common.Symbol> Symbols { get; } = Enum.GetValues<Common.Symbol>();
+}
---
+++ samples/Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FluentIcons.Samples.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
